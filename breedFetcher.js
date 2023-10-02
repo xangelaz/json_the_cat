b16fb6685ac1c2f69eff2c0264d72199fb49dee0
Angela Zhou <a03zhou@gmail.com>
2023-10-02 01:24:07 -0400

    add index and test file

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,18 +1,38 @@
 const request = require('request');
-let breed = process.argv[2];
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
-  if (error) {
-    console.log('error:', error); // Print the error if one occurred
-    return;
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    console.log(`Breed ${breed} not found`);
-    return;
-  }
-  console.log('first entry of data:', data[0].description);
-});
+const fetchBreedDescription = function(breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
+    if (error) {
+      return callback(error, null);
+    }
+    const data = JSON.parse(body);
+    if (data.length === 0) {
+      return callback('That cat does not exist', null);
+    }
+    if (data[0].description) {
+      return callback(null, data[0].description);
+    }
+  });
+};
 
-//can use JSON.parse to convert the JSON string into an actual object^.
 
+// original code below
+// const request = require('request');
+// let breed = process.argv[2];
+
+// request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+//   if (error) {
+//     console.log('error:', error); // Print the error if one occurred
+//     return;
+//   }
+//   const data = JSON.parse(body);
+//   if (data.length === 0) {
+//     console.log(`Breed ${breed} not found`);
+//     return;
+//   }
+//   console.log('first entry of data:', data[0].description);
+// });
+
+// can use JSON.parse to convert the JSON string into an actual object^.
+
+module.exports = { fetchBreedDescription }; 
---
+++ index.js
@@ -0,0 +1,11 @@
+const { fetchBreedDescription } = require('./breedFetcher');
+let breedName = process.argv[2];
+
+fetchBreedDescription(breedName, (error, desc) => {
+  if (error) {
+    console.log('Error fetch details:', error);
+  } else {
+    console.log(desc);
+  }
+});
+
